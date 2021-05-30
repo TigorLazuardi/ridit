5d3a7957fe49acfe8df2dd6814d0e8913956e371
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-05-30 19:56:07 +0700

    shortened read config

--- src/app/config/mod.rs
+++ src/app/config/mod.rs
@@ -3,25 +3,21 @@ pub mod model;
 pub mod sort;
 pub mod thread;
 
+use anyhow::{Context, Result};
 use model::Config;
-use std::error::Error;
 use std::fs;
 use toml;
 
-pub fn read_config() -> Result<Config, Box<dyn Error>> {
+pub fn read_config() -> Result<Config> {
     let c: Config;
     if let Ok(p) = default::get_relative_config_path() {
-        let content =
-            fs::read(p).or_else(|err| Err(format!("failed to read riddit.toml: {}", err)))?;
+        let content = fs::read(&p)
+            .with_context(|| format!("failed to read riddit.toml from {}", p.display()))?;
         c = toml::from_slice(content.as_ref())?;
     } else {
         let p = default::get_xdg_config_path()?;
-        let content = fs::read(p).or_else(|err| {
-            Err(format!(
-                "failed to read riddit.toml from global config path: {}",
-                err
-            ))
-        })?;
+        let content = fs::read(&p)
+            .with_context(|| format!("failed to read riddit.toml on {}", p.display()))?;
         c = toml::from_slice(content.as_ref())?;
     }
     Ok(c)
